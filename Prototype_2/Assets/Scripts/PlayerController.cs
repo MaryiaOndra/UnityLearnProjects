69e48422246e70930984028713937bc0d98ffd30
Mary Ondra <vidra48@mail.ru>
2020-09-27 18:01:25 +0300

    finished step1

---
+++ Prototype_2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 20f;
+    public float xRange= 15f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // keep the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3( -xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput *  Time.deltaTime * speed);
+    }
+}
