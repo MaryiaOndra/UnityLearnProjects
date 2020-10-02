778e25793efd1d4e241b1ec28892c926f38da9cf
Maryia Ondra <maryiaondra@gmail.com>
2020-10-02 15:20:16 +0300

    Added BallGame and Removed prototype

---
+++ BallGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject completeLevelUI;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private string tagName = "PickUp";
+    private static int count;
+    private int countPickUpObjects;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        countPickUpObjects = GameObject.FindGameObjectsWithTag(tagName).Length;
+        completeLevelUI.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 momementVector = movementValue.Get<Vector2>();
+        movementX = momementVector.x;
+        movementY = momementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "COUNT: " + count.ToString();
+
+        if (count >= countPickUpObjects)
+        {
+            completeLevelUI.SetActive(true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(tagName))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+}
---
+++ BallGame/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private float rtX = 15f; 
+    private float rtY = 30f; 
+    private float rtZ = 45f; 
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(rtX, rtY, rtZ) * Time.deltaTime);
+    }
+}
