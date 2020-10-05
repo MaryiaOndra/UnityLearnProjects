5dd59546f0c6ccab02d76af87ce40fdf9190b296
Maryia Ondra <maryiaondra@gmail.com>
2020-10-05 22:09:54 +0300

    started Challenge_3

--- Cwc-W4_S1/Assets/Scripts/PlayerController.cs
+++ Cwc-W4_S1/Assets/Scripts/PlayerController.cs
@@ -3,26 +3,34 @@
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRB;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+
     public float jumpForce;
     public float gravityModifier;
-
-    private Animator playerAnim;
     public float walkSpeed = 5f;
 
     public bool isRunning = false;
     public bool isOnGround = true;
     public bool isGameOver = false;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+
     //private int currentJump = 0;
     //private const int maxJumps = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        leftPos = transform.position;
-
         playerRB = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,6 +39,7 @@ public class PlayerController : MonoBehaviour
         if (!isRunning)
         {
             transform.Translate(Vector3.forward * walkSpeed);
+            dirtParticle.Stop();
         }
 
         if (Input.GetKey(KeyCode.Space) && !isGameOver &&(isOnGround /*|| maxJumps > currentJump)*/))
@@ -39,15 +48,30 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
             //make player jump when you press space
             playerAnim.SetTrigger("Jump_trig");
-            //currentJump++;           
+            //currentJump++;      
+
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Start"))
+        {
+            isRunning = true;
+            playerAnim.SetFloat("Speed_f", 1f);
+
+            //GameObject startObject = GameObject.FindGameObjectWithTag("Start");
+            Destroy(GameObject.FindGameObjectWithTag("Start"));
+
+            explosionParticle.Stop();
+            dirtParticle.Play();
+        }
+        else if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
             //currentJump = 0;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
@@ -58,11 +82,10 @@ public class PlayerController : MonoBehaviour
             //add death animation
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
-        }
-        else if (collision.gameObject.CompareTag("Start"))
-        {
-            isRunning = true;
-            playerAnim.SetFloat("Speed_f", 1f);
+
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
