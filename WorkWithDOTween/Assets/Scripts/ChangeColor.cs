3f03d76717d38927861e4b5693935476c851d1d6
Maryia Ondra <maryiaondra@gmail.com>
2020-10-30 19:10:52 +0300

    worked with DOTWeen

---
+++ WorkWithDOTween/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.DOColor(Color.green, 2f).SetLoops(-1, LoopType.Yoyo);
+        _spriteRenderer.DOFade(0, 0.5f).SetLoops(-1, LoopType.Yoyo);
+    }
+}
---
+++ WorkWithDOTween/Assets/Scripts/DoMove.cs
@@ -0,0 +1,10 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class DoMove : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DOMove(new Vector3(0, 5, 0), 3).SetLoops(-1, LoopType.Yoyo).From();
+    }
+}
