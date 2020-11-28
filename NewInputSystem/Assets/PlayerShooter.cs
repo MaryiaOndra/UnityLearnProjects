9fef73f99aab339aeae485ad274bea911499cd86
Maryia Ondra <maryiaondra@gmail.com>
2020-11-28 23:29:18 +0300

    started work with new InputSystem

---
+++ NewInputSystem/Assets/PlayerShooter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof (PlayerInput))]
+public class PlayerShooter : MonoBehaviour
+{
+    public void OnShoot(InputAction.CallbackContext context) 
+    {
+        Debug.Log("FIRE!");
+    }
+}
